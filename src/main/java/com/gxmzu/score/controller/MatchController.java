24f7f201f95d8272311c119601d8715c82537b21
‘gengzipeng’ <568673694@qq.com>
2022-11-12 15:03:24 +0800

    对比赛Match的增删改

--- src/main/java/com/gxmzu/score/controller/MatchController.java
+++ src/main/java/com/gxmzu/score/controller/MatchController.java
@@ -3,12 +3,13 @@ package com.gxmzu.score.controller;
 import com.gxmzu.score.domain.AjaxResult;
 import com.gxmzu.score.domain.entity.Match;
 import com.gxmzu.score.service.MatchService;
+import com.gxmzu.score.utils.HttpStatus;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -34,5 +35,61 @@ public class MatchController extends BaseController{
         startPage(match);
         List<Match> list = matchService.getMatchList(match);
         return getDataTable(list);
+    }
+
+    /**
+     * 添加比赛信息
+     *
+     * @param match 比赛信息
+     * @return
+     */
+    @PostMapping("/add")
+    public AjaxResult addMatch(HttpServletRequest httpServletRequest, @RequestBody Match match){
+        //判断传来的参数是否为空
+        if(match.getMatchName().isEmpty()||match.getInfo().isEmpty()||match.getScoreRuleName().isEmpty()||match.getCreateBy().isEmpty()){
+            return AjaxResult.error(HttpStatus.LACK_QUERY,"缺少请求参数");
+        //判断传来的最高分是否合法
+        }else if(match.getMaxScore() <= 0){
+            return AjaxResult.error(HttpStatus.ERROR_QUERY,"请求参数有误");
+        //参数无误后插入数据，并执行业务操作
+        }else{
+        return matchService.addMatch(httpServletRequest, match);
+        }
+    }
+
+    /**
+     * 更新比赛信息
+     *
+     * @param match 比赛信息
+     * @return
+     */
+    @PutMapping("/update")
+    public AjaxResult updateMatch(HttpServletRequest httpServletRequest, @RequestBody Match match){
+        //判断传来的最高分是否合法
+        if(match.getMaxScore() <= 0){
+            return AjaxResult.error(HttpStatus.ERROR_QUERY,"请求参数有误");
+        //参数无误后插入数据，并执行业务操作
+        }else{
+            return matchService.updateMatch(httpServletRequest,match);
+        }
+    }
+
+    /**
+     * 删除比赛信息
+     *
+     * @param matchId 删除的比赛id
+     * @return
+     */
+    @DeleteMapping("/{matchId}")
+    public AjaxResult deleteMatch(HttpServletRequest httpServletRequest, @PathVariable Integer matchId){
+        //判断参数matchId是否合法
+        if(matchId == null){
+            return AjaxResult.error(HttpStatus.LACK_QUERY,"缺少请求参数");
+        }else if(matchId <= 0){
+            return AjaxResult.error(HttpStatus.ERROR_QUERY,"请求参数有误");
+        //参数无误后执行业务操作
+        }else{
+            return matchService.deleteMatch(httpServletRequest, matchId);
+        }
     }
 }
--- src/main/java/com/gxmzu/score/mapper/MatchMapper.java
+++ src/main/java/com/gxmzu/score/mapper/MatchMapper.java
@@ -2,6 +2,8 @@ package com.gxmzu.score.mapper;
 
 import com.gxmzu.score.domain.entity.Match;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -20,4 +22,28 @@ public interface MatchMapper {
      * @return 比赛列表
      */
     public List<Match> selectMatchList(Match match);
+
+    /**
+     * 添加比赛信息
+     *
+     * @param match
+     * @return 返回自增id
+     */
+    public int addMatch(Match match);
+
+    /**
+     * 修改比赛信息
+     *
+     * @param match
+     * @return
+     */
+    public int updateMatch(Match match);
+
+    /**
+     * 删除比赛信息
+     *
+     * @param matchId 比赛id
+     * @return
+     */
+    public int deleteMatch(@Param("matchId") Integer matchId);
 }
