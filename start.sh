db448be92af922e13ee810cce89d2942446c9ab7
flow2000 <1982989137@qq.com>
2022-11-19 13:15:56 +0800

    action自动部署配置

---
+++ start.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# 使用方法：修改jar包名称，完整的jar路径，运行日志名称即可
+
+#jar包名称
+APP_NAME=score-0.0.1-SNAPSHOT.jar
+#完整的jar路径
+APP_PATH=target/$APP_NAME
+#运行日志
+LOG_FILE=score.log
+#JVM运行参数
+JVM_OPTS="-Xms128m -Xmx512m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m -Dfile.encoding=UTF-8"
+
+#使用说明，用来提示输入参数
+usage() {
+    echo "Usage: sh 执行脚本.sh [start|stop|restart|status]"
+    exit 1
+}
+
+#检查程序是否在运行
+is_exist(){
+  pid=`ps -ef|grep $APP_NAME|grep -v grep|awk '{print $2}' `
+  #如果不存在返回1，存在返回0
+  if [ -z "${pid}" ]; then
+   return 10
+  else
+    return 0
+  fi
+}
+
+#启动方法
+start(){
+  is_exist
+  if [ $? -eq "0" ]; then
+    echo "${APP_NAME} is already running. pid=${pid} ."
+  else
+    nohup java $JVM_OPTS -jar $APP_PATH  > $LOG_FILE 2>&1 &
+    echo "${APP_NAME} started"
+  fi
+}
+
+#停止方法
+stop(){
+  is_exist
+  if [ $? -eq "0" ]; then
+    kill -9 $pid
+    echo "${APP_NAME} stoped"
+  else
+    echo "${APP_NAME} is not running"
+  fi
+}
+
+#输出运行状态
+status(){
+  is_exist
+  if [ $? -eq "0" ]; then
+    echo "${APP_NAME} is running. Pid is ${pid}"
+  else
+    echo "${APP_NAME} is NOT running."
+  fi
+}
+
+#重启
+restart(){
+  status
+  stop
+  start
+  status
+}
+
+#根据输入参数，选择执行对应方法，不输入则执行使用说明
+case "$1" in
+  "start")
+    start
+    ;;
+  "stop")
+    stop
+    ;;
+  "status")
+    status
+    ;;
+  "restart")
+    restart
+    ;;
+  *)
+    usage
+    ;;
+esac
